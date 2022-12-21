bf086844b0f120a4e81b71713be4e0b73f7b155f
SimpleSandman <simple.sandman@gmail.com>
2022-12-20 21:57:55 -0600

    Added "meta" SQLite support

--- UmaMusumeLoadSqlData/Program.cs
+++ UmaMusumeLoadSqlData/Program.cs
@@ -22,17 +22,19 @@ namespace UmaMusumeLoadSqlData
     public class Program
     {
         #region Private Class Variables
-        private static readonly List<SqliteMasterRecord> _sqliteTableNames = new List<SqliteMasterRecord>();
-        private static readonly List<SqliteMasterRecord> _sqliteIndexNames = new List<SqliteMasterRecord>();
-        private static readonly List<DataTable> _sqliteDataTables = new List<DataTable>();
         private static readonly SqliteUtility _sqliteUtility = new SqliteUtility();
         private static readonly SqlServerUtility _sqlServerUtility = new SqlServerUtility();
         private static readonly MySqlUtility _mySqlUtility = new MySqlUtility();
         private static readonly ConcurrentDictionary<string, string> _textDictionary = new ConcurrentDictionary<string, string>();
         private static bool _hadBulkInsertError = false;
-        private static string _masterDbFilepath = @$"{Environment.CurrentDirectory}\master.mdb";
+        #endregion
+
+        #region Private Const Variables
         private const string TRANSLATED_REPO_NAME = "noccu/umamusu-translate";
         private const string TRANSLATED_BRANCH_NAME = "master";
+
+        private const string SOURCE_MASTER_FILENAME = "master/master.mdb";
+        private const string SOURCE_META_FILENAME = "meta";
         #endregion
 
         #region Command Line Arg Class Variables
@@ -71,53 +73,45 @@ namespace UmaMusumeLoadSqlData
 
             try
             {
+                string masterDbDestinationFilepath = @$"{Environment.CurrentDirectory}\master.mdb";
+                string metaDbDestinationFilepath = @$"{Environment.CurrentDirectory}\meta";
+
                 if (AspNetCoreEnvironment == "Development" && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    _masterDbFilepath 
+                    masterDbDestinationFilepath 
                         = @$"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\AppData\LocalLow\Cygames\umamusume\master\master.mdb";
+                    metaDbDestinationFilepath
+                        = @$"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\AppData\LocalLow\Cygames\umamusume\meta";
                 }
                 else
                 {
-                    // Download master.mdb file from remote GitHub repo
-                    await GithubUtility.DownloadRemoteFileAsync(_repoName, _branchName, "master.mdb", _masterDbFilepath);
+                    // Download meta and master.mdb files from remote GitHub repo
+                    await GithubUtility.DownloadRemoteFileAsync(_repoName, _branchName, SOURCE_MASTER_FILENAME, masterDbDestinationFilepath);
+                    await GithubUtility.DownloadRemoteFileAsync(_repoName, _branchName, SOURCE_META_FILENAME, metaDbDestinationFilepath);
                 }
 
                 /* Verify master.mdb exists */
-                if (File.Exists(_masterDbFilepath))
+                if (File.Exists(masterDbDestinationFilepath) && File.Exists(metaDbDestinationFilepath))
                 {
-                    Console.WriteLine("SUCCESS: Found master.mdb");
+                    Console.WriteLine("SUCCESS: Found meta and master.mdb files");
                 }
                 else
                 {
-                    Console.WriteLine("FATAL ERROR: Cannot find master.mdb");
+                    Console.WriteLine("FATAL ERROR: Cannot find meta and master.mdb files");
                     CloseProgram();
                 }
 
-                /* Pull table info from the SQLite master.mdb */
-                using (SQLiteConnection connection = new SQLiteConnection($"Data Source={_masterDbFilepath}"))
-                {
-                    connection.Open();
-
-                    // Get raw data table names needed to loop
-                    _sqliteUtility.SqliteTableNames(connection, _sqliteTableNames);
-
-                    // Get raw data index names needed to loop
-                    _sqliteUtility.SqliteIndexScript(connection, _sqliteIndexNames);
-
-                    // Loop through each table and pull all available data
-                    _sqliteUtility.LoadSqliteDataTables(connection, _sqliteTableNames, _sqliteDataTables);
-                }
+                await LoadDatabaseData(masterDbDestinationFilepath, SOURCE_MASTER_FILENAME);
+                await LoadDatabaseData(metaDbDestinationFilepath, SOURCE_META_FILENAME);
 
                 /* Import data from SQLite into the provided database(s) below */
                 if (!string.IsNullOrEmpty(_mySqlConnectionString) && _mySqlConnectionString != "N/A")
                 {
-                    await SqlDestinationAsync<MySqlConnection, MySqlCommand>(_mySqlConnectionString).ConfigureAwait(false);
                     await LoadEnglishTranslationsAsync<MySqlConnection, MySqlCommand>(_mySqlConnectionString).ConfigureAwait(false);
                 }
 
                 if (!string.IsNullOrEmpty(_sqlServerConnectionString) && _sqlServerConnectionString != "N/A")
                 {
-                    await SqlDestinationAsync<SqlConnection, SqlCommand>(_sqlServerConnectionString).ConfigureAwait(false);
                     await LoadEnglishTranslationsAsync<SqlConnection, SqlCommand>(_sqlServerConnectionString).ConfigureAwait(false);
                 }
 
@@ -143,7 +137,43 @@ namespace UmaMusumeLoadSqlData
         #endregion
 
         #region Private Methods
-        private static async Task SqlDestinationAsync<T, U>(string connectionString) 
+        private static async Task LoadDatabaseData(string dbFilepath, string sourceFilename)
+        {
+            List<SqliteMasterRecord> sqliteMasterTableNames = new List<SqliteMasterRecord>();
+            List<SqliteMasterRecord> sqliteMasterIndexNames = new List<SqliteMasterRecord>();
+            List<DataTable> sqliteMasterDataTables = new List<DataTable>();
+
+            /* Pull table info from the SQLite master.mdb */
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFilepath}"))
+            {
+                connection.Open();
+
+                // Get raw data table names needed to loop
+                _sqliteUtility.SqliteTableNames(connection, sqliteMasterTableNames);
+
+                // Get raw data index names needed to loop
+                _sqliteUtility.SqliteIndexScript(connection, sqliteMasterIndexNames);
+
+                // Loop through each table and pull all available data
+                _sqliteUtility.LoadSqliteDataTables(connection, sqliteMasterTableNames, sqliteMasterDataTables);
+            }
+
+            /* Import data from SQLite into the provided database(s) below */
+            if (!string.IsNullOrEmpty(_mySqlConnectionString) && _mySqlConnectionString != "N/A")
+            {
+                await SqlDestinationAsync<MySqlConnection, MySqlCommand>(sourceFilename, _mySqlConnectionString,
+                    sqliteMasterTableNames, sqliteMasterIndexNames, sqliteMasterDataTables).ConfigureAwait(false);
+            }
+
+            if (!string.IsNullOrEmpty(_sqlServerConnectionString) && _sqlServerConnectionString != "N/A")
+            {
+                await SqlDestinationAsync<SqlConnection, SqlCommand>(sourceFilename, _sqlServerConnectionString,
+                    sqliteMasterTableNames, sqliteMasterIndexNames, sqliteMasterDataTables).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task SqlDestinationAsync<T, U>(string sourceFilename, string connectionString, 
+            List<SqliteMasterRecord> sqliteMasterTableNames, List<SqliteMasterRecord> sqliteMasterIndexNames, List<DataTable> sqliteMasterDataTables) 
             where T : IDbConnection, new()
             where U : IDbCommand, new()
         {
@@ -154,11 +184,11 @@ namespace UmaMusumeLoadSqlData
                 if (typeof(T) == typeof(MySqlConnection))
                 {
                     tableSchema = "";
-                    Console.WriteLine("\nAttempting to load table data into a MySQL/MariaDB database...\n");
+                    Console.WriteLine($"\nAttempting to load \"{sourceFilename}\" table data into a MySQL/MariaDB database...\n");
                 }
                 else
                 {
-                    Console.WriteLine("\nAttempting to load table data into a SQL Server database...\n");
+                    Console.WriteLine($"\nAttempting to load \"{sourceFilename}\" table data into a SQL Server database...\n");
                 }
 
                 /* Push new info into destination database */
@@ -168,28 +198,32 @@ namespace UmaMusumeLoadSqlData
                     destinationConnection.Open();
 
                     // Check if there are any new SQLite tables that need to be created
+                    string tablePrefix = sourceFilename == SOURCE_META_FILENAME 
+                        ? "meta_" 
+                        : "";
+
                     List<string> sqlTableNames = SelectTableNames(destinationConnection);
                     foreach (string name in sqlTableNames)
                     {
-                        _sqliteTableNames.RemoveAll(t => t.TableName == name);
+                        sqliteMasterTableNames.RemoveAll(t => tablePrefix + t.TableName == name);
                     }
 
                     // Alert the owner of any new tables with their indexes
-                    if (_sqliteTableNames.Count > 0)
+                    if (sqliteMasterTableNames.Count > 0)
                     {
-                        Console.WriteLine($"WARNING: {_sqliteTableNames.Count} new table(s) found from the master.mdb file...");
+                        Console.WriteLine($"WARNING: {sqliteMasterTableNames.Count} new table(s) found from the \"{sourceFilename}\" file...");
                         Console.WriteLine("-----------------------------------------------------------");
 
                         // NOTE: Local dev strings used and displayed
                         string scaffoldContent = "Scaffold-DbContext 'User Id=root;Password=sa;Host=localhost;Database=umamusume;Character Set=utf8mb4' "
                                 + $"Pomelo.EntityFrameworkCore.MySql -OutputDir Models -ContextDir Context -T ";
 
-                        foreach (SqliteMasterRecord table in _sqliteTableNames)
+                        foreach (SqliteMasterRecord table in sqliteMasterTableNames)
                         {
                             Console.WriteLine();
 
                             Console.WriteLine($"{table.SqlScript};");
-                            foreach (SqliteMasterRecord index in _sqliteIndexNames.Where(i => i.TableName == table.TableName))
+                            foreach (SqliteMasterRecord index in sqliteMasterIndexNames.Where(i => i.TableName == table.TableName))
                             {
                                 Console.WriteLine($"{index.SqlScript};");
                             }
@@ -203,27 +237,29 @@ namespace UmaMusumeLoadSqlData
                         Console.WriteLine("\n-----------------------------------------------------------\n");
 
                         // Remove sqliteDataTables that don't exist in the destination table yet
-                        _sqliteDataTables.RemoveAll(t => _sqliteTableNames.Any(n => n.TableName == t.TableName));
+                        sqliteMasterDataTables.RemoveAll(t => sqliteMasterTableNames.Any(n => n.TableName == t.TableName));
                     }
 
-                    foreach (DataTable sqliteDataTable in _sqliteDataTables.OrderBy(t => t.TableName))
+                    foreach (DataTable sqliteDataTable in sqliteMasterDataTables.OrderBy(t => t.TableName))
                     {
+                        string tableName = $"{tableSchema}{tablePrefix}{sqliteDataTable.TableName}";
+
                         // Start with a clean slate
                         using (U truncateCommand = new U())
                         {
-                            truncateCommand.CommandText = $"TRUNCATE TABLE {tableSchema}{sqliteDataTable.TableName}";
+                            truncateCommand.CommandText = $"TRUNCATE TABLE {tableName}";
                             truncateCommand.Connection = destinationConnection;
                             truncateCommand.ExecuteNonQuery();
                         }
 
                         // Load DataTable into the destination database
-                        if (!await TryBulkInsertDataTableAsync(destinationConnection, sqliteDataTable.TableName, sqliteDataTable))
+                        if (!await TryBulkInsertDataTableAsync(destinationConnection, tableName, sqliteDataTable))
                         {
                             // Look at the SQLite table and find the missing columns for the destination table
-                            _hadBulkInsertError = AreMissingColumnsResolved<T, U>(destinationConnection, sqliteDataTable, tableSchema);
+                            _hadBulkInsertError = AreMissingColumnsResolved<T, U>(sourceFilename, destinationConnection, sqliteDataTable, tableSchema);
 
                             // Try to insert again with the newly added columns
-                            if (!await TryBulkInsertDataTableAsync(destinationConnection, sqliteDataTable.TableName, sqliteDataTable, false))
+                            if (!await TryBulkInsertDataTableAsync(destinationConnection, tableName, sqliteDataTable, false))
                             {
                                 _hadBulkInsertError = true;
                             }
@@ -237,13 +273,13 @@ namespace UmaMusumeLoadSqlData
             }
         }
 
-        private static bool AreMissingColumnsResolved<T, U>(T destinationConnection, DataTable sqliteDataTable, string tableSchema)
+        private static bool AreMissingColumnsResolved<T, U>(string dbFilepath, T destinationConnection, DataTable sqliteDataTable, string tableSchema)
             where T : IDbConnection, new()
             where U : IDbCommand, new()
         {
             try
             {
-                using (SQLiteConnection sqliteDebugConnection = new SQLiteConnection($"Data Source={_masterDbFilepath}"))
+                using (SQLiteConnection sqliteDebugConnection = new SQLiteConnection($"Data Source={dbFilepath}"))
                 {
                     sqliteDebugConnection.Open();
 
